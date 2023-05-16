7055449b909a2d0be81e7a66e5adb91268481230
Zadd ZC <davidbk927@gmail.com>
2023-05-16 13:51:42 +0200

    done shit

--- Matador/Construction.cs
+++ Matador/Construction.cs
@@ -1,16 +1,161 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Matador
+﻿namespace Matador
 {
     internal class Construction
     {
     }
+    public class Player
+    {
+        public List<Property> Properties { get; set; }
+        public int Money { get; set; }
+        public bool IsBankrupt { get; set; }
+        public Color Color { get; set; }
+        public int Position { get; set; }
+
+        public Player(Color color)
+        {
+            Properties = new List<Property>();
+            Money = 0;
+            IsBankrupt = false;
+            Color = color;
+            Position = 0;
+        }
+        public int GetTotalValue()
+        {
+            int value = Money;
+            for (int i = 0; i < Properties.Count; i++)
+                value += Properties[i].GetValue();
+
+            return value;
+        }
+    }
+    public class Design
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Group Group { get; set; }
+        public string Special { get; set; }
+
+        public Design(string name, Color color, Group group)
+        {
+            Name = name;
+            Color = color;
+            Group = group;
+            Special = "empty";
+        }
+        public Design(string name, Color color, Group group, string special)
+        {
+            Name = name;
+            Color = color;
+            Group = group;
+            Special = special;
+        }
+    }
+    public enum Group
+    {
+        start,
+        blue,
+        chance,
+        skat,
+        boat,
+        orange,
+        soda,
+        green,
+        gray,
+        red,
+        white,
+        prison,
+        parking,
+        yellow,
+        purple,
+    }
     public class Property
     {
+        public int Level { get; set; }
+        public int Position { get; private set; }
+        public Player Owner { get; private set; }
+        public bool IsPurchasable { get; set; }
+        public int LevelCost { get; private set; }
+        public int PurchaseCost { get; private set; }
+        public bool IsPansat { get; set; }
+        public Design Design { get; set; }
+        public Property(int position, bool isPurchaseable, int levelCost, int purchaseCost, Design design)
+        {
+            Level = 0;
+            Position = position;
+            Owner = MainPage.bank;
+            IsPurchasable = isPurchaseable;
+            LevelCost = levelCost;
+            PurchaseCost = purchaseCost;
+            IsPansat = false;
+            Design = design;
+        }
 
+        public void UnPansæt(Player player)
+        {
+            int value = GetPansatValue();
+            if (player.Money >= value && IsPansat == true)
+            {
+                player.Money -= value;
+                IsPansat = false;
+            }
+        }
+        public void Pansæt(Player player)
+        {
+            if (Level != 0)
+                LevelDown(player, Level);
+
+            if (IsPansat == false)
+            {
+                player.Money += GetPansatValue();
+                IsPansat = true;
+            }
+        }
+        public int GetPansatValue()
+        {
+            return PurchaseCost / 2;
+        }
+        public int GetValue()
+        {
+            return PurchaseCost + LevelCost * Level;
+        }
+        public bool LevelUp(Player player) // returns wheter it was succesful or not
+        {
+            bool isSuccesfull = false;
+            if (player.Money >= LevelCost && Level < 5)
+            {
+                player.Money -= LevelCost;
+                Level++;
+                isSuccesfull = true;
+            }
+            return isSuccesfull;
+        }
+        public bool LevelDown(Player player) // returns the value of the freed up money
+        {
+            bool isSuccesfull = false;
+            if (Level > 0)
+            {
+                player.Money += LevelCost;
+                Level--;
+                isSuccesfull = true;
+            }
+
+            return isSuccesfull;
+        }
+        public bool LevelDown(Player player, int byHowMuch) // returns the value of the freed up money
+        {
+            bool isSuccesfull = false;
+            for (int i = 0; i < byHowMuch; i++)
+            {
+                if (Level > 0)
+                {
+                    player.Money += LevelCost;
+                    Level--;
+                    isSuccesfull = true;
+                }
+                else
+                    break;
+            }
+            return isSuccesfull;
+        }
     }
 }
--- Matador/MainPage.xaml.cs
+++ Matador/MainPage.xaml.cs
@@ -6,6 +6,10 @@ namespace Matador
     public partial class MainPage : ContentPage
     {
         public static Grid Board { get; private set; }
+        public static List<Player> playerList = new List<Player>();
+        public static List<Player> playerOrder = new List<Player>();
+        public static List<Property> properties = new List<Property>();
+        public static Player bank = new Player(Colors.Orange);
 
         public MainPage()
         {
@@ -13,6 +17,7 @@ namespace Matador
             Board = board;
 
             LineMan();
+            InitPropiteis();
 
         }
         public static void LineMan()
@@ -22,8 +27,6 @@ namespace Matador
             for (int i = 40 - 1; i >= 0; i--)
             {
                 double angle = 45 - i * 9;
-
-
 
                 Line theLine = new Line
                 {
@@ -62,17 +65,58 @@ namespace Matador
             Debug.WriteLine(id);
 
         }
-        public static void InitPropiteis()
+        public static void InitPlayers()
         {
 
         }
-        
+
 
         public static double DegToRad(double angle)
         {
             return angle * Math.PI / 180d;
         }
+        public static void InitPropiteis()
+        {
+            properties.Add(new Property(0, false, -1, -1, new Design("Start", Colors.Red, Group.start)));
+            properties.Add(new Property(1, true, 1000, 1200, new Design("Rødovervej", Colors.LightBlue, Group.blue)));
+            properties.Add(new Property(2, false, -1, -1, new Design("Chance", Colors.Black, Group.chance)));
+            properties.Add(new Property(3, true, 1000, 1200, new Design("Hvidovervej", Colors.LightBlue, Group.blue)));
+            properties.Add(new Property(4, false, -1, -1, new Design("TAX:4000", Colors.Green, Group.skat, "4000")));
+            properties.Add(new Property(5, true, -1, 4000, new Design("firstplaceholder", Colors.Blue, Group.boat)));
+            properties.Add(new Property(6, true, 1000, 2000, new Design("Roskildevej", Colors.Orange, Group.orange)));
+            properties.Add(new Property(7, false, -1, -1, new Design("Chance", Colors.Black, Group.chance)));
+            properties.Add(new Property(8, true, 1000, 2000, new Design("Valby Langgade", Colors.Orange, Group.orange)));
+            properties.Add(new Property(9, true, 1000, 2400, new Design("Allegade", Colors.Orange, Group.orange)));
+            properties.Add(new Property(10, false, -1, -1, new Design("Prison", Colors.Black, Group.prison, "Cell")));
+            properties.Add(new Property(11, true, 2000, 2800, new Design("FrediksBerg Alle", Colors.Green, Group.green)));
+            properties.Add(new Property(12, true, -1, 3000, new Design("Squash", Colors.Orange, Group.soda)));
+            properties.Add(new Property(13, true, 2000, 2800, new Design("Büllovsvej", Colors.Green, Group.green)));
+            properties.Add(new Property(14, true, 2000, 3200, new Design("Gl. Kongevej", Colors.Green, Group.green)));
+            properties.Add(new Property(15, true, -1, 4000, new Design("MolsLinjen", Colors.Red, Group.boat)));
+            properties.Add(new Property(16, true, 2000, 3600, new Design("Bernstofsvej", Colors.Gray, Group.gray)));
+            properties.Add(new Property(17, false, -1, -1, new Design("Chance", Colors.Black, Group.chance)));
+            properties.Add(new Property(18, true, 2000, 3600, new Design("Hellerupsvej", Colors.Gray, Group.gray)));
+            properties.Add(new Property(19, true, 2000, 4000, new Design("Strandvej", Colors.Gray, Group.gray)));
+            properties.Add(new Property(20, false, -1, -1, new Design("Parking", Colors.AliceBlue, Group.parking)));
+            properties.Add(new Property(21, true, 3000, 4400, new Design("Trianglen", Colors.Red, Group.red)));
+            properties.Add(new Property(22, false, -1, -1, new Design("Chance", Colors.Black, Group.chance)));
+            properties.Add(new Property(23, true, 3000, 4400, new Design("Østergade", Colors.Red, Group.red)));
+            properties.Add(new Property(24, true, 3000, 4800, new Design("Grøningen", Colors.Red, Group.red)));
+            properties.Add(new Property(25, true, -1, 4000, new Design("thirdplaceholder", Colors.Blue, Group.boat)));
+            properties.Add(new Property(26, true, 3000, 5200, new Design("Bredgade", Colors.White, Group.white)));
+            properties.Add(new Property(27, true, 3000, 5200, new Design("Kgs Nytorv", Colors.White, Group.white)));
+            properties.Add(new Property(28, true, -1, 3000, new Design("Cola", Colors.DarkRed, Group.soda)));
+            properties.Add(new Property(29, true, 3000, 5600, new Design("Østergade", Colors.White, Group.white)));
+            properties.Add(new Property(30, false, -1, -1, new Design("GotoPrison", Colors.Black, Group.prison, "gotoPrison")));
+            properties.Add(new Property(31, true, 4000, 6000, new Design("Amergertorv", Colors.Yellow, Group.yellow)));
+            properties.Add(new Property(32, true, 4000, 6000, new Design("Vimmelskaftet", Colors.Yellow, Group.yellow)));
+            properties.Add(new Property(33, false, -1, -1, new Design("Chance", Colors.Black, Group.chance)));
+            properties.Add(new Property(34, true, 4000, 6400, new Design("Nygade", Colors.Yellow, Group.yellow)));
+            properties.Add(new Property(35, true, -1, 4000, new Design("fourthplaceholder", Colors.Blue, Group.boat)));
+            properties.Add(new Property(36, false, -1, -1, new Design("Chance", Colors.Black, Group.chance)));
+            properties.Add(new Property(37, true, 4000, 7000, new Design("Frediksberg gade", Colors.Purple, Group.purple)));
+            properties.Add(new Property(38, false, -1, -1, new Design("TAX:2000", Colors.Green, Group.skat, "2000")));
+            properties.Add(new Property(39, true, 4000, 8000, new Design("Rådhuspladsen", Colors.Purple, Group.purple)));
+        }
     }
 }
-
-
